a0e9ddc7c5181f2d7fe0add3daf81306feea7f07
LaithAlsahori <laithmousa2005cs@gmail.com>
2026-03-17 00:51:26 +0300

    Add files via upload
    pointers and a struct. The project is designed to help beginners understand how to work with dynamic memory allocation, 2D arrays, and functions in C++.
    
    Features:
    
    Dynamic Array Creation
    The user inputs the number of rows and columns, and the program allocates memory dynamically for a 2D array.
    
    Filling the Array
    The program fills the array with consecutive integers to demonstrate traversal and indexing in a 2D array.
    
    Printing the Array
    The array is displayed in a formatted table using setw for alignment.
    
    Use of Structs
    The 2D array and its dimensions are stored in a struct, showing how to organize related data in C++.
    
    Memory Management
    The project introduces basic dynamic memory allocation using new and the importance of proper memory handling (deletion after use is recommended).

---
+++ 2D array.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+struct StrThe_Data
+{
+	int row = 0;
+	int col = 0;
+	int **arr;
+
+};
+void FunCreate_2D_Array(StrThe_Data &The_Data)
+{
+	cout << "enter the number of row : ";
+	cin >> The_Data.row;
+
+	cout << "enter the number of column : ";
+	cin >> The_Data.col;
+
+	The_Data.arr = new int* [The_Data.row];
+
+	for (int i=0;i < The_Data.row;i++)
+	{
+		The_Data.arr[i] = new int[The_Data.col];
+	}
+}
+void FunFill_2D_Array(StrThe_Data& The_Data)
+{
+	FunCreate_2D_Array(The_Data);
+
+	int test = 0;
+
+	for (int i = 0; i < The_Data.row; i++)
+	{
+		for (int j = 0; j < The_Data.col; j++)
+		{
+			test += 1;
+
+			The_Data.arr[i][j] = test;
+		}
+	}
+}
+void FunPrint_The_Array(StrThe_Data& The_Data)
+{
+	FunFill_2D_Array(The_Data);
+
+	for (int i = 0; i < The_Data.row; i++)
+	{
+		for (int j = 0; j < The_Data.col; j++)
+		{
+			cout <<setw(15)<< The_Data.arr[i][j] << "  ";
+		}
+		cout << endl;
+	}
+}
+int main()
+{
+	StrThe_Data The_Data;
+
+	FunPrint_The_Array(The_Data);
+
+	return 0;
+}
